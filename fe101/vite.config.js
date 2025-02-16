e1211daf5f8cc5ea1f16170bbd101a7d2af6ef85
alvydas-vaitkus <alv.vait@outlook.com>
2025-02-16 15:32:08 +0000

    changed vite config base

--- fe101/vite.config.js
+++ fe101/vite.config.js
@@ -5,4 +5,5 @@ import commonjs from 'vite-plugin-commonjs'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), commonjs()],
+  base: 'https://al-vait1235711.github.io/react-t0:8080/'
 })
