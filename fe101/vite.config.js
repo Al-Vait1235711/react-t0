77286e0269c66fcd12932fbae6e1e2f559e53da5
Al V <alv.vait@outlook.com>
2025-03-05 13:13:12 +0000

    Update vite.config.js

--- fe101/vite.config.js
+++ fe101/vite.config.js
@@ -5,5 +5,5 @@ import commonjs from 'vite-plugin-commonjs'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), commonjs()],
-  base: '/react-t0/'
+  base: '/single-page-react-app/'
 })
