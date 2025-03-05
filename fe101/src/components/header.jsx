87066e5bab4beabb9add454d5b1946cb2bd9c48e
Al V <alv.vait@outlook.com>
2025-03-05 13:17:21 +0000

    Update header.jsx

--- fe101/src/components/header.jsx
+++ fe101/src/components/header.jsx
@@ -10,7 +10,7 @@ export default function AppHeader(){
     return(
         <Navbar expand="sm" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Cubicx</Navbar.Brand>
+        <Navbar.Brand href="#home">SomeLogo</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
